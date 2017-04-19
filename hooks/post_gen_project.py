6bae6cb8bc12b140752586eac0b999f280407370
Johannes Alneberg <alneberg@kth.se>
2017-04-19 10:57:59 +0200

    Removed most, restarting

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,51 +5,27 @@ import argparse
 import os
 import logging
 
-COMMIT = "{{cookiecutter.toolbox_commit}}"
 GITHUB_PROJECT_REMOTE = "{{cookiecutter.github_project_remote}}"
 
 WORKFLOW_REPO = "{{cookiecutter.snakemake_workflows_repo}}"
-WORKFLOW_COMMIT = "{{cookiecutter.workflow_commit}}"
+SUBWORKFLOW_NAME = "{{cookiecutter.snakemake_subworkflow_name}}" 
 
 TOOLBOX_GITHUB_REPO = "{{cookiecutter.toolbox_github_repo}}"
-TOOLBOX_COMMIT = "{{cookiecutter.toolbox_commit}}" 
-
-LOGFILE_LOCATION = os.path.expanduser("{{cookiecutter.logfile_location}}")
 
 def main(args):
-    # Create logfile with link to main.log within repo
-    if os.path.isfile(LOGFILE_LOCATION):
-        raise Exception("Logfile already exists, please delete it and start over")
-
-    open(LOGFILE_LOCATION, 'w').close()
-    os.symlink(LOGFILE_LOCATION, 'main.log')
-
-    logging.basicConfig(format='%(asctime)s %(message)s', filename='main.log', level=logging.INFO) 
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO) 
     os.system('git init')
     os.system('git remote add origin {0}'.format(GITHUB_PROJECT_REMOTE))
     logging.info("Initalized git repo with remote {0}".format(GITHUB_PROJECT_REMOTE))
 
     # Add toolbox as submodule
     os.system('git submodule add https://github.com/{} toolbox'.format(TOOLBOX_GITHUB_REPO))
-    os.chdir("toolbox")
-    os.system('git checkout {0} && echo {0}'.format(TOOLBOX_COMMIT))
-    os.chdir("..")
-
-    # The checkout might have left the toolbox dirty
-    os.system("git add toolbox")
-    os.system('git commit -m "Added toolbox submodule"') 
     logging.info("Added toolbox repo {0} at {1} as submodule.".format(TOOLBOX_GITHUB_REPO, TOOLBOX_COMMIT))
 
     # Add snakemake-workflows as submodule
     os.system('git submodule add https://github.com/{} snakemake-workflows'.format(WORKFLOW_REPO))
-    os.chdir("snakemake-workflows")
-    os.system('git checkout {0}'.format(WORKFLOW_COMMIT))
-    os.chdir("..")
-    
-    # The checkout might have left the snakemake-workflows dirty
-    os.system("git add snakemake-workflows")
-    os.system('git commit -m "Added snakemake-workflows submodule"') 
-
+    subworkflow_files = os.path.join("snakemake-workflows", SUBWORKFLOW_NAME, "*")
+    os.symlink(subworkflow_files, ".")
     logging.info("Added snakemake-workflows repo {0} at {1} as submodule.".format(WORKFLOW_REPO, WORKFLOW_COMMIT))
 
 if __name__ == "__main__":
